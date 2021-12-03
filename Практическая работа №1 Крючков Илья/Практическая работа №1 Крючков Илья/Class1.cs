c7d4a3de3b3ca54f5e76f22cfdf11064394c02b3
IlyaKruck <95465158+IlyaKruck@users.noreply.github.com>
2021-12-03 13:27:48 +0300

    Add files via upload

---
+++ Практическая работа №1 Крючков Илья/Практическая работа №1 Крючков Илья/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib_5
+{
+    class Class1
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="k"></param>
+        /// <param name="kol"></param>
+        /// <param name="h2"></param>
+        /// <param name="h1"></param>
+
+        public static void Sum(int k, out int kol, out string h2, out string h1)//Считает сумму
+        {
+            int x;// сгенерированное число
+            int sum = 0;// сумма
+            kol = 0;// количество сгенерированных чисел
+            Random rnd = new Random();
+            h1 = "";// строка для вывода суммы
+            h2 = "";// строка для вывода сгенерированных чисел
+
+            while (sum > k)// пока сумма больше к
+            {
+                x = rnd.Next(-7, 3);// генерирование чисел, распределенных в диапазоне от -7 до 3
+                h2 += Convert.ToString(x) + " ";
+                kol++;// счетчик количества чисел
+                sum = sum + x;// вычисление суммы    
+            }
+            h1 = Convert.ToString(sum);
+
+        }
+    }
+}
---
+++ Практическая работа №1 Крючков Илья/Практическая работа №1 Крючков Илья/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Lib_5;
+
+namespace Практическая_работа__1_Крючков_Илья
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Сброс_Click(object sender, RoutedEventArgs e)
+        {
+            Chisla.Text = null;
+            Sum.Text = null;
+            Kol.Text = null;
+            K.Text = null;
+        }
+
+        private void Опрограмме_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Крючков Илья\n" + "Практическая работа №1\n" + "Вычислить сумму целых случайных чисел, распределенных в диапазоне от -7 до 3, пока эта сумма не превышает некоторого числа K. Вывести на экран сгенерированные числа, значение суммы, и количество сгенерированных чисел.");
+        }
+
+        private void Выход_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Raschet_Click(object sender, RoutedEventArgs e)
+        {
+            int k, kol;
+            k = Convert.ToInt32(K.Text);// ввод K
+            if (k > 0) MessageBox.Show("Ошибка, введите отрицательное число");
+            else
+            {
+                Class1.Sum(k, out kol, out string h2, out string h1);// применение функции
+                Chisla.Text = h2;// вывод сгенерированных чисел
+                Sum.Text = h1;// вывод суммы
+                Kol.Text = Convert.ToString(kol);// вывод количесва чисел
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Крючков Илья\n" + "Практическая работа №1\n" + "Вычислить сумму целых случайных чисел, распределенных в диапазоне от -7 до 3, пока эта сумма не превышает некоторого числа K. Вывести на экран сгенерированные числа, значение суммы, и количество сгенерированных чисел.");
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Chisla.Text = null;
+            Sum.Text = null;
+            Kol.Text = null;
+            K.Text = null;
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Крючков Илья\n" + "Практическая работа №1\n" + "Вычислить сумму целых случайных чисел, распределенных в диапазоне от -7 до 3, пока эта сумма не превышает некоторого числа K. Вывести на экран сгенерированные числа, значение суммы, и количество сгенерированных чисел.");
+        }
+    }
+}
